163783e5488d581d8772de85a4e8037b0ff3ba3a
周明 <mmix1009@163.com>
2018-09-29 11:40:22 +0800

    Add main header

---
+++ ZMMacros/Classes/ZMMacro.h
@@ -0,0 +1,17 @@
+//
+//  ZMMacro.h
+//  Pods
+//
+//  Created by 周明 on 2018/9/29.
+//
+
+#ifndef ZMMacro_h
+#define ZMMacro_h
+
+#import "colorMacro.h"
+#import "direcotryMacro.h"
+#import "frameMacro.h"
+#import "logicMacro.h"
+#import "xibMacro.h"
+
+#endif /* ZMMacro_h */
